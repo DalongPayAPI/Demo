4657d7b6b61a27b44cddda01b2879a2848fd3b19
DalongPayAPI <37768016+DalongPayAPI@users.noreply.github.com>
2018-03-25 10:50:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+require_once "payapi_crypt.php";
+
+$apiKey    = "11111111";
+$clientKey = "22222222";
+
+//Получаем сессионный ключ
+$cryptSessionObject = getSessionKey($clientKey);
+$SessionObject = \PAYAPI\cryptLib::decrypt($cryptSessionObject, $clientKey);
+$session = JSON_decode($SessionObject);
+$sessionKey = $session->key;
+
+//Формируем крипто-ключ
+$cryptoKey = $apiKey.$sessionKey.$clientKey;
+
+//ПРИЕМ ПЛАТЕЖА
+
+//Формируем счет на оплату (параметр запроса)
+$receipt  = new stdClass();
+$receipt->currency = "LTC";
+$receipt->amount = "0.005";
+$receiptStr = JSON_encode($receipt);
+
+//Шифруем параметры платежа
+$cryptoReceiptStr = \PAYAPI\cryptLib::crypt($buyStr, $cryptoKey);
+
+//Выполняем вызов функции АПИ и  получаем зашифрованный результат
+$cryptoReceiptResult = Receipt($sessionKey, $cryptoReceiptStr);
+
+//Расшифровываем результат
+$ReceiptResult = \PAYAPI\cryptLib::decrypt($cryptoReceiptResult, $cryptoKey);
+
+//Получаем объект Чек
+$ReceiptObject = JSON_decode(ReceiptResult);
+
+//Получаем инфу из объекта (ИД чека)
+$recieptID = $ReceiptObject->receipt;
+
+
+//СТАТУС ПЛАТЕЖА
+//Формируем параметр запроса
+//В чачестве параметра используем ИД чека, сформированного на предыдущем шаге
+$param  = new stdClass();
+$param->receipt = $recieptID;
+$paramStr = JSON_encode($param);
+//Шифруем параметр запроса
+$cryptoParamStr = \PAYAPI\cryptLib::crypt($paramStr, $cryptoKey);
+//Выполняем вызов функции, получаем шифрованный результат
+$cryptReceitpResult = Receipt($sessionKey, $cryptoParamStr);
+//Расшифровываем результат
+$ReceiptResult = \PAYAPI\cryptLib::decrypt($cryptReceitpResult, $cryptoKey);
+echo $ReceiptResult;
+
+
+
+function getSessionKey($key) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL,"https://test-payapi.dalongpay.com/v1/");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'm=init&q=\'{"key": "'.$key.'"}\'');
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec ($ch);
+
+    curl_close ($ch);
+    return $server_output;
+}
+
+
+function Receipt($key, $param) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL,"https://test-payapi.dalongpay.com/v1/");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'm=get&q=\'{"key": "'.$key.'", "param":"'.urlencode($param).'"}\'');
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec ($ch);
+
+    curl_close ($ch);
+    return $server_output;
+}
+
+
---
+++ payapi_crypt.php
@@ -0,0 +1,32 @@
+<?php
+namespace PAYAPI;
+
+class cryptLib
+{
+    public static function crypt($text, $key) {
+        $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+        $iv = openssl_random_pseudo_bytes($ivlen);
+        $ciphertext_raw = openssl_encrypt($text, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+        $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+        $crypt_text = base64_encode( $iv.$hmac.$ciphertext_raw );
+        return $crypt_text;
+    }
+
+    public static function decrypt($crypt_text, $key) {
+        $text = '';
+
+        $c = base64_decode($crypt_text);
+        $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+        $iv = substr($c, 0, $ivlen);
+        $hmac = substr($c, $ivlen, $sha2len=32);
+        $ciphertext_raw = substr($c, $ivlen+$sha2len);
+        $plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv);
+        $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
+        if (hash_equals($hmac, $calcmac))
+        {
+            $text =  $plaintext;
+        }
+
+        return $text;
+    }
+}
